a3b0cf50f69ba3e58ae6fc7129602e034d8773ec
kotzuwei <owo6289@gmail.com>
2025-01-08 04:16:50 +0800

    0108

--- Web/main.js
+++ Web/main.js
@@ -1,45 +1,23 @@
-// const app = Vue.createApp({}) //創建一個 Vue 應用程式實例，{}定義應用的全局屬性
-
-// //將ProjectList的全局元件註冊到應用中，HTML中使用<project-list></project-list>插入元件
-// app.component('project-carousel', ProjectCarousel);
-// // app.component('works-carousel', WorksCarousel);
-
-// app.mount('#app')
-
-
-
-
-// main.js
-// import { createApp } from "vue";
-// import ProjectCarousel from "./components/ProjectCarousel.js"; // 原有的元件
-// import WorksCarousel from "./components/WorksCarousel.js"; // 新增的元件
-
-// const app = createApp({});
-// app.component("project-carousel", ProjectCarousel);
-// app.component("works-carousel", WorksCarousel); // 註冊新元件
-// app.mount("#app");
-
-
-
-
+// 引入 Vue 組件，渲染到頁面
 import ProjectCarousel from './components/ProjectCarousel.js';
 import WorksCarousel from './components/WorksCarousel.js';
 
+
+// 創建 Vue 應用
 const app = Vue.createApp({});
 
+
+// 註冊 Vue 組件，使用<project-carousel>、<works-carousel>標籤
 app.component("project-carousel", ProjectCarousel);
 app.component("works-carousel", WorksCarousel);
 
 
+// index.html、Works.html ㄉcarousel分別是精選作品與全部作品
+// 檢查是否有 id 為 "app" 或 "works-app" 的元素，並將 Vue 應用掛載到相應的元素上
 if (document.getElementById("app")) {
-    app.mount("#app");  // 如果在 index.html 中掛載
+    // 如果在 index.html 中有 id 為 "app" 的元素，將 Vue 應用掛載到該元素
+    app.mount("#app");
 } else if (document.getElementById("works-app")) {
-app.mount("#works-app");  // 如果在 Works.html 中掛載
+    // 如果在 Works.html 中有 id 為 "works-app" 的元素，將 Vue 應用掛載到該元素
+    app.mount("#works-app");
 }
-
-
-
-
-
-
-
